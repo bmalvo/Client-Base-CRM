b3028b1aff0546a8023a9fb3b88301ddbe0ca5ef
bmalvo <boydmalvo@gmail.com>
2024-05-07 12:31:31 +0200

    set clientRouter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const {clientRouter} = require('./routers/client')
 
 const app = express();
 
@@ -9,8 +10,14 @@ app.use(express.urlencoded({
 
 app.use(express.static('public'))
 app.use(express.json());
-app.engine('.hbs', hbs({
+app.engine('.hbs', hbs.engine({
     extname: '.hbs',
     // helpers: handlebarsHelpers,
 }));
 app.set('view engine', '.hbs');
+
+app.use('/client', clientRouter)
+
+app.listen(3000, 'localhost', () => {
+    console.log('Listening on http://localhost:3000');
+});
