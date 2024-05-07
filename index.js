a8810c01f3444e33fb52bc7f98fa66fc2f72cec9
bmalvo <boydmalvo@gmail.com>
2024-05-07 12:00:22 +0200

    configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(express.static('public'))
+
+app.engine('.hbs', hbs({
+    extname: '.hbs',
+    // helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
