bb98bb688582ccc9912ddb019accbbbcd1ff9670
bmalvo <boydmalvo@gmail.com>
2024-05-15 20:08:47 +0200

    building list-all view for clients

--- routers/client.js
+++ routers/client.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const { db } = require('../utils/db');
 
 const clientRouter = express.Router();
 
 clientRouter
     .get('/', (req, res) => {
-        res.send('it works');
-  })
+        res.render('client/list-all', {
+            clients: db.getAll()
+        });
+    })
+    .get('/:id', (req, res) => {
+        res.send('get one');
+    })
+    .post('/', (req, res) => {
+        res.send('add');
+    })
+    .put('/:id', (req, res) => {
+        res.send('modificate')
+    })
+    .delete('/:id', (req, res) => {
+        res.send('delete');
+    })
 
 module.exports = {
     clientRouter,
